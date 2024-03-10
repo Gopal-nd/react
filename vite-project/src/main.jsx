0fe959fbc8632a9c94042482c26e31df41b15f2a
Gopal N D <gopalhelpyou@gmail.com>
2024-03-10 14:55:50 +0530

    project4

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import Navbar from "./pages/Navbar.jsx";
 import { BrowserRouter, Route, Link ,Routes, Router} from "react-router-dom";
 import "./index.css";
+import Slider from "./pages/Slider.jsx";
 import RandomColor from "./pages/RandomColor.jsx";
 import { ThemeProvider } from "@/components/theme-provider";
 import Accordian from "./pages/accordian.jsx";
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
         <Route path="/" element={<App/>} />
         <Route path="/accordian" element={<Accordian/>} />
         <Route path="/randomcolor" element={<RandomColor/>} />
+        <Route  path="/slider" element={<Slider/>} /> 
         </Routes>
       </BrowserRouter>
     </ThemeProvider>
